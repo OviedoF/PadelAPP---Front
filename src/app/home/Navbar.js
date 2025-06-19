6bb66d7903f560828b39db2708abe69fd593c7aa
OviedoF <oviedofederico39@gmail.com>
2025-06-19 17:17:51 -0300

    Q

--- src/app/home/Navbar.js
+++ src/app/home/Navbar.js
@@ -114,16 +114,6 @@ export default function Nav() {
             </Link>
           </div>
 
-          <div className="hidden sm:ml-6 sm:flex sm:items-center">
-            <Link
-              href={routes.register}
-              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-cyan-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            >
-              <FaSearch className="w-5 h-5 mr-2" />
-              Buscar Jugador
-            </Link>
-          </div>
-
           <div className="-mr-2 flex items-center sm:hidden">
             <button
               onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
@@ -173,15 +163,6 @@ export default function Nav() {
                 )}
               </div>
             ))}
-            <div className="px-3 py-2">
-              <Link
-                href="/registro"
-                className="block text-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-cyan-500 hover:bg-blue-700"
-              >
-                <FaSearch className="inline-block w-5 h-5 mr-2" />
-                Buscar Jugador
-              </Link>
-            </div>
           </div>
         </div>
       )}
